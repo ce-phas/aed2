4be883c69818640a9a7776f10e61aa648893756d
Pedro Amorim Sa <pedrohasa@gmail.com>
2021-08-23 23:49:40 -0300

    create tp1(9)

--- pra/tp1/9_arquivo/Arquivo.java
+++ pra/tp1/9_arquivo/Arquivo.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * Arquivo - programa
  *
@@ -7,20 +9,17 @@
  */
 public class Arquivo
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        // definir dados
-        String title;
-
-        // ler dados
-        title = MyIO.readLine();
-
-        while (!(title.length() == 3 &&
-                 title.charAt(0) == 'F' &&
-                 title.charAt(1) == 'I' &&
-                 title.charAt(2) == 'M'))
+        try
         {
-            title = MyIO.readLine();
+            RandomAccessFile file = new RandomAccessFile("pub.txt", "r");
+            file.seek(0);
+            MyIO.println(file.getFilePointer());
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
         }
     }
 }

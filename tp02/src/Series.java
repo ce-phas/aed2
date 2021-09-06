88e7e67f397f32a146a63ceaa084481120f37ae9
Pedro Amorim Sa <pedrohasa@gmail.com>
2021-09-06 01:59:12 -0300

    add clone() method, rearrange class files

---
+++ tp02/src/Series.java
@@ -0,0 +1,142 @@
+public class Series {
+    // definir atributos
+    private String nome;
+    private String formato;
+    private String duracao;
+    private String paisDeOrigem;
+    private String idiomaOriginal;
+    private String emissoraDeTelevisao;
+    private String transmissaoOriginal;
+    private int numeroTemporadas;
+    private int numeroEpisodios;
+
+    // construtores
+    Series() {}
+
+    Series(String nome, String formato, String duracao, String paisDeOrigem,
+           String idiomaOriginal, String emissoraDeTelevisao, String transmissaoOriginal,
+           int numeroTemporadas, int numeroEpisodios) {
+
+        this.nome = nome;
+        this.formato = formato;
+        this.duracao = duracao;
+        this.paisDeOrigem = paisDeOrigem;
+        this.idiomaOriginal = idiomaOriginal;
+        this.emissoraDeTelevisao = emissoraDeTelevisao;
+        this.transmissaoOriginal = transmissaoOriginal;
+        this.numeroTemporadas = numeroTemporadas;
+        this.numeroEpisodios = numeroEpisodios;
+    }
+
+    // metodos gets
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getFormato() {
+        return this.formato;
+    }
+
+    public String getDuracao() {
+        return this.duracao;
+    }
+
+    public String getPaisDeOrigem() {
+        return this.paisDeOrigem;
+    }
+
+    public String getIdiomaOriginal() {
+        return this.idiomaOriginal;
+    }
+
+    public String getEmissoraDeTelevisao() {
+        return this.emissoraDeTelevisao;
+    }
+
+    public String getTransmissaoOriginal() {
+        return this.transmissaoOriginal;
+    }
+
+    public int getNumeroTemporadas() {
+        return this.numeroTemporadas;
+    }
+
+    public int getNumeroEpisodios() {
+        return this.numeroEpisodios;
+    }
+
+    // metodos sets
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public void setPaisDeOrigem(String paisDeOrigem) {
+        this.paisDeOrigem = paisDeOrigem;
+    }
+
+    public void setIdiomaOriginal(String idiomaOriginal) {
+        this.idiomaOriginal = idiomaOriginal;
+    }
+
+    public void setEmissoraDeTelevisao(String emissoraDeTelevisao) {
+        this.emissoraDeTelevisao = emissoraDeTelevisao;
+    }
+
+    public void setTransmissaoOriginal(String transmissaoOriginal) {
+        this.transmissaoOriginal = transmissaoOriginal;
+    }
+
+    public void setNumeroTemporadas(int numeroTemporadas) {
+        this.numeroTemporadas = numeroTemporadas;
+    }
+
+    public void setNumeroEpisodios(int numeroEpisodios) {
+        this.numeroEpisodios = numeroEpisodios;
+    }
+
+    // metodo clone
+    public Series clone() {
+        Series clone = new Series();
+
+        clone.setNome(this.nome);
+        clone.setFormato(this.formato);
+        clone.setDuracao(this.duracao);
+        clone.setPaisDeOrigem(this.paisDeOrigem);
+        clone.setIdiomaOriginal(this.idiomaOriginal);
+        clone.setEmissoraDeTelevisao(this.emissoraDeTelevisao);
+        clone.setTransmissaoOriginal(this.transmissaoOriginal);
+        clone.setNumeroTemporadas(this.numeroTemporadas);
+        clone.setNumeroEpisodios(this.numeroEpisodios);
+
+        return clone;
+    }
+
+    // metodo imprimir
+    public void imprimir() {
+        MyIO.println(nome + " " + formato + " " + duracao + " " + paisDeOrigem
+                     + " " + idiomaOriginal + " " + emissoraDeTelevisao + " "
+                     + transmissaoOriginal + " " + numeroTemporadas + " "
+                     + numeroEpisodios);
+    }
+
+    // metodo ler
+    public void ler() {
+        this.nome = MyIO.readLine();
+        this.formato = MyIO.readLine();
+        this.duracao = MyIO.readLine();
+        this.paisDeOrigem = MyIO.readLine();
+        this.idiomaOriginal = MyIO.readLine();
+        this.emissoraDeTelevisao = MyIO.readLine();
+        this.transmissaoOriginal = MyIO.readLine();
+        this.numeroTemporadas = MyIO.readInt();
+        this.numeroEpisodios = MyIO.readInt();
+    }
+}
